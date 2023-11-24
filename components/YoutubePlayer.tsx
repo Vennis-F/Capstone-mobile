7d27aba7f117491ed1e621b3b23b15c040d718b8
Hoang Loc <nguyenhoangloc1403@gmail.com>
2023-11-24 19:30:39 +0700

    Question

--- components/YoutubePlayer.tsx
+++ components/YoutubePlayer.tsx
@@ -1,15 +1,16 @@
 import * as React from 'react';
-import {Text, StyleSheet, View,SafeAreaView,FlatList,TouchableOpacity} from 'react-native';
+import { Text, StyleSheet, View, SafeAreaView, FlatList, TouchableOpacity, Image } from 'react-native';
 
 import { Video, ResizeMode } from 'expo-av';
 import { Checkbox, HStack } from 'native-base';
-import  Icon  from 'react-native-vector-icons/MaterialIcons';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { COLORS } from '../libs/const/color';
 const Courses = [
   {
     id: "1",
     title: "Bài 1 : Cách vẽ con bò",
     time: "4 phút",
-   
+
   },
   {
     id: "2",
@@ -21,57 +22,96 @@ const Courses = [
     id: "3",
     title: "Bài 3",
     time: "5 phút",
-    
+
   },
 ];
 const Questions = [
   {
     id: "1",
-    question: "Tôi là ai?",
+    username: "Nguyen Hoang Loc",
+    title: "Bài này hay quá ạ",
+    content: 'Bài giảng số 1',
+    time: '24-11-2023'
   },
   {
     id: "2",
-    question: "Mày là ai?",
+    username: "Nguyen Hoang Anh",
+    title: "Bài bổ ích quá",
+    content: 'Bài giảng số 1',
+    time: '10-11-2023'
+  },
+  {
+    id: "3",
+    username: "Nguyen Minh Khôi",
+    title: "Bài 10 điểm",
+    content: 'Bài giảng số 1',
+    time: '01-11-2023'
+  },
+  {
+    id: "4",
+    username: "Nguyen Minh Khôi",
+    title: "Bài 10 điểm",
+    content: 'Bài giảng số 1',
+    time: '01-11-2023'
   },
 ];
 const QuestionItem = ({ item, onPress, backgroundColor, textColor }) => (
   <TouchableOpacity
     onPress={onPress}
-    style={[styles.item, { backgroundColor, paddingHorizontal: 20 }]}
+    style={[styles.item, { backgroundColor, paddingHorizontal: 20, borderRadius: 10, marginHorizontal: 20, marginBottom: 10 }]}
   >
-    {/* Render your question item as needed */}
-    <Text style={{ fontSize: 20, fontWeight: "bold" }}>{item.question}</Text>
+    <View style={styles.question}>
+      <View>
+        <Image
+          style={styles.tinyLogo}
+          source={{
+            uri: "https://reactnative.dev/img/tiny_logo.png",
+          }}
+        />
+      </View>
+      <View style={{ marginLeft: -50 }}>
+        <Text style={{ fontSize: 19, fontWeight: "bold" }}>{item.username}</Text>
+        <Text style={{ fontSize: 14 }}>{item.title}</Text>
+      </View>
+      <View>
+        <Icon name='chat' size={22} />
+      </View>
+    </View>
+    <View style={styles.questionSub}>
+      <Text>{item.content} - </Text>
+      <Text>{item.time}</Text>
+    </View>
   </TouchableOpacity>
 );
 
 const Item = ({ item, onPress, backgroundColor, textColor }) => (
   <TouchableOpacity
     onPress={onPress}
-    style={[styles.item, { backgroundColor ,paddingHorizontal:20}]}
+    style={[styles.item, { backgroundColor, paddingHorizontal: 20 }]}
   >
-      <View style={styles.box}>
-        <View style={{marginRight:20, marginTop:16}}>
-          <Checkbox shadow={2} value='test' >
-          </Checkbox>
-        </View>
-        <View >
-          <Text style={{fontSize:20,fontWeight:"bold"}}>{item.title}</Text>
-          <View style={styles.boxSub}>
-           <Icon name='ondemand-video' size={20}/> 
-            <Text style={{fontSize:15,marginLeft:10}}>{item.time}</Text>
-          </View>
+    <View style={styles.box}>
+      <View style={{ marginRight: 20, marginTop: 16 }}>
+        <Checkbox shadow={2} value='test' >
+        </Checkbox>
+      </View>
+      <View >
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>{item.title}</Text>
+        <View style={styles.boxSub}>
+          <Icon name='ondemand-video' size={20} />
+          <Text style={{ fontSize: 15, marginLeft: 10 }}>{item.time}</Text>
         </View>
       </View>
-    </TouchableOpacity>
+    </View>
+  </TouchableOpacity>
 )
 export default function YoutubePlayer() {
-  const categories = ["Nội dung khóa học","Đặt câu hỏi"]
+  const categories = ["Nội dung khóa học", "Đặt câu hỏi"]
   const video = React.useRef(null);
-  const [categoryIndex, setCategoryIndex] =React.useState(0)
+  const [categoryIndex, setCategoryIndex] = React.useState(0)
   const [status, setStatus] = React.useState({});
   const [selectedId, setSelectedId] = React.useState();
   const renderQuestionItem = ({ item }) => {
-    const backgroundColor = item.id === selectedId ? "#CECADA" : "#fff";
+    const backgroundColor = item.id === selectedId ? "#CECADA" : COLORS.GRAY_300;
     const color = item.id === selectedId ? "white" : "black";
 
     return (
@@ -86,18 +126,18 @@ export default function YoutubePlayer() {
   const CategoryList = () => {
     return (
       <View style={styles.categoryContainer}>
-        {categories.map((item,index)=> (
-          <TouchableOpacity key={index} onPress={() =>setCategoryIndex(index)}>
-          <Text key={index} style={[styles.categoryText, categoryIndex == index && styles.categoryTextSelected]}>{item}</Text>
+        {categories.map((item, index) => (
+          <TouchableOpacity key={index} onPress={() => setCategoryIndex(index)}>
+            <Text key={index} style={[styles.categoryText, categoryIndex == index && styles.categoryTextSelected]}>{item}</Text>
           </TouchableOpacity>
         ))}
       </View>
-      )
-    }
+    )
+  }
   const renderItem = ({ item }) => {
     const backgroundColor = item.id === selectedId ? "#CECADA" : "#fff";
     const color = item.id === selectedId ? "white" : "black";
-    
+
     return (
       <Item
         item={item}
@@ -119,7 +159,7 @@ export default function YoutubePlayer() {
         resizeMode={ResizeMode.CONTAIN}
         isLooping
         onPlaybackStatusUpdate={status => setStatus(() => status)}
-        
+
       />
       {/* <View >
         <Text style={{fontSize:20,fontWeight:'bold',paddingHorizontal:20,marginBottom:15}}>Nội dung khóa học</Text>
@@ -148,38 +188,56 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     // justifyContent: 'center',
-    backgroundColor: '#FFF',
-    width:'100%'
+    backgroundColor: COLORS.WHITE,
+    width: '100%'
   },
   video: {
     width: "100%",
     height: 250,
-   
+
   },
-  box:{
-    marginBottom:15,
-    flexDirection:'row',
-    justifyContent :'flex-start'
+  box: {
+    marginBottom: 15,
+    flexDirection: 'row',
+    justifyContent: 'flex-start'
   },
-  boxSub:{
-    flexDirection:'row',
-    justifyContent :'flex-start'
+  boxSub: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start'
   },
-  categoryContainer:{
-    flexDirection:"row",
-    justifyContent:"space-between",
-    paddingHorizontal:20
+  categoryContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 20
   },
-  categoryText:{
-    fontSize:20,
-    fontWeight:"bold",
-    marginBottom:20
+  categoryText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20
   },
-  categoryTextSelected:{
-    color:'#FB641B',
-    paddingBottom:5,
-    borderBottomWidth:2,
-    borderColor: '#FB641B',
+  categoryTextSelected: {
+    color: COLORS.RED_500,
+    paddingBottom: 5,
+    borderBottomWidth: 2,
+    borderColor: COLORS.RED_500,
+  },
+  tinyLogo: {
+    width: 45,
+    height: 45,
+    borderRadius: 60,
+    marginTop: 5
+  },
+  question: {
+    flexDirection: 'row',
+    justifyContent: "space-between",
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  questionSub: {
+    flexDirection: 'row',
+    justifyContent: "flex-start",
+    marginLeft: 60,
+    paddingBottom: 10
   }
 
 });
---
+++ libs/const/color.ts
@@ -0,0 +1,10 @@
+export enum COLORS {
+    RED_400 = '#f87171',
+    RED_450 = '#e64e4e',
+    RED_500 = '#ef4444',
+    RED_600 = '#dc2626',
+    YELLOW_500 = '#eab308',
+    GRAY_300 = '#d1d5db',
+    BLACK = '#2D2F31',
+    WHITE = '#FFFFFF',
+}
