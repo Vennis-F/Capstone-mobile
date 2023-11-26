973fd94c26708f319963534005a8ff555089ae13
NPTho <97748149+NPTho@users.noreply.github.com>
2023-11-27 00:47:03 +0700

    Update handle-token.ts

--- libs/core/handle-token.ts
+++ libs/core/handle-token.ts
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import JWT from 'expo-jwt';
 
 const TOKEN_KEY = '@access_token'; // Key dùng để lưu trữ token trong AsyncStorage
-const JWT_SECRET = "c25T7Azx9MdX8VBye7yNBFynBBmQEECcvarf22eeng49fBbEbaGqwL6c8efAZA7";
+const JWT_SECRET = "c25T7Azx9MdX8VBye7yNBFynBBmQEECcvarf22eeng49fBbEbaGqwL6c8efAZA7"
 
 export enum UserRole {
   CUSTOMER = 'Customer',
@@ -14,10 +14,10 @@ export enum UserRole {
 }
 
 export type UserInfor = {
-  id: string;
-  email?: string;
-  username?: string;
-  role: UserRole;
+  id: string
+  email?: string
+  username?: string
+  role: UserRole
 };
 
 export const decodeToken = (accessToken: string): UserInfor => JWT.decode(accessToken, JWT_SECRET) as UserInfor;
